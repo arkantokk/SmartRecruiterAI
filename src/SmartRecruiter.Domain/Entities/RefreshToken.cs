a51a6931c05c42b491df1e8b907547beaf975d85
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-04-08 11:05:15 +0200

    Created new entity for refresh tokens

---
+++ src/SmartRecruiter.Domain/Entities/RefreshToken.cs
@@ -0,0 +1,28 @@
+﻿namespace SmartRecruiter.Domain.Entities;
+
+public class RefreshToken
+{
+    public Guid Id { get; private set; }
+    public string Token { get; private set; }
+    public DateTime ExpiryDate { get; private set; }
+    public bool IsRevoked { get; private set; }
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    
+    public string UserId { get; private set; }
+
+    public RefreshToken(string token, DateTime expiryDate, string userId)
+    {
+        Id = Guid.NewGuid();
+        Token = token;
+        ExpiryDate = expiryDate;
+        IsRevoked = false;
+        UserId = userId;
+    }
+
+    public void Revoke()
+    {
+        IsRevoked = true;
+    }
+    
+    
+}
