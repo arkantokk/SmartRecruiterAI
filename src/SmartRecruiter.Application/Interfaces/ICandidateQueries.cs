634a9acd51b37b1456795ce528180ddf32abbfd8
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-04-10 13:53:53 +0200

    Added a new method to get candidates for user id

--- src/SmartRecruiter.Application/Interfaces/ICandidateQueries.cs
+++ src/SmartRecruiter.Application/Interfaces/ICandidateQueries.cs
@@ -5,4 +5,5 @@ namespace SmartRecruiter.Application.Interfaces;
 public interface ICandidateQueries
 {
     Task<PagedResponse<CandidateDto>> GetCandidatesForVacancyIdAsync(Guid vacancyId, int pageNumber, int pageSize);
+    Task<PagedResponse<CandidateDto>> GetCandidatesForUserAsync(string userId, int pageNumber, int pageSize);
 }
--- src/SmartRecruiter.Infrastructure/Services/CandidateReadService.cs
+++ src/SmartRecruiter.Infrastructure/Services/CandidateReadService.cs
@@ -55,4 +55,44 @@ public class CandidateReadService : ICandidateQueries
                 )).ToList();
         return new PagedResponse<CandidateDto>(items, totalCount, pageNumber, pageSize);
     }
+
+    public async Task<PagedResponse<CandidateDto>> GetCandidatesForUserAsync(string userId, int pageNumber, int pageSize)
+    {
+        var baseQuery = _context.Candidates
+            .AsNoTracking()
+            .Where(c => _context.JobVacancies.Any(v => v.UserId == userId));
+        var totalCount = await baseQuery.CountAsync();
+        var candidates = await baseQuery
+            .OrderByDescending(c => c.Evaluation.Score)
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .Select(c => new
+                {
+                    c.Id,
+                    c.FirstName,
+                    c.LastName,
+                    c.Email,
+                    c.ResumeUrl,
+                    Score = c.Evaluation != null ? c.Evaluation.Score : 0,
+                    Summary = c.Evaluation != null ? c.Evaluation.Summary : string.Empty,
+                    Skills = c.Evaluation != null ? c.Evaluation.Skills : new List<string>(),
+                    Status = c.Status.ToString()
+                }
+            )
+            .ToListAsync();
+        var items = candidates
+            .Select(c => new CandidateDto(
+                c.Id,
+                c.FirstName,
+                c.LastName,
+                c.Email,
+                c.ResumeUrl != null ? _storageService.GenerateReadOnlyUrl(c.ResumeUrl, TimeSpan.FromHours(1)) : null,
+                c.Score,
+                c.Summary,
+                c.Skills,
+                c.Status
+            )).ToList();
+        return new PagedResponse<CandidateDto>(items, totalCount, pageNumber, pageSize);
+
+    }
 }
