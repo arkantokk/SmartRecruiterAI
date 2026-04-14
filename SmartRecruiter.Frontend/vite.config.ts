767d8cddae895cfa611dade221d7c746ac78d499
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-04-14 10:55:03 +0200

    Added frontend tests

--- SmartRecruiter.Frontend/vite.config.ts
+++ SmartRecruiter.Frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: '../src/SmartRecruiter.API/wwwroot',
     emptyOutDir: true,
+  },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
   }
 })
