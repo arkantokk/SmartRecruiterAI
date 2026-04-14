85c8055e4951ba5de23f1b362c4dd9ceaeb0f187
arkantokk <145562523+arkantokk@users.noreply.github.com>
2026-04-14 10:06:30 +0200

    Added tests for validator and candidate service

---
+++ SmartRecruiter.Application.Tests/Services/CandidateServiceTests.cs
@@ -0,0 +1,89 @@
+﻿using SmartRecruiter.Application.Interfaces;
+using SmartRecruiter.Domain.Interfaces;
+using Moq;
+using SmartRecruiter.Application.DTOs;
+using SmartRecruiter.Application.Services;
+using SmartRecruiter.Domain.DTOs;
+using SmartRecruiter.Domain.Entities;
+using Xunit;
+
+namespace SmartRecruiter.Application.Tests.Services;
+
+public class CandidateServiceTests
+{
+    private readonly Mock<ICandidateRepository> _repository;
+    private readonly Mock<IJobVacancyRepository> _vacancyRepository;
+    private readonly Mock<IAiService> _aiService;
+    private readonly Mock<IStorageService> _storageService;
+    private readonly Mock<ICandidateQueries> _candidateQueries;
+
+    private readonly CandidateService _service;
+
+
+    public CandidateServiceTests()
+    {
+        _repository = new Mock<ICandidateRepository>();
+        _vacancyRepository = new Mock<IJobVacancyRepository>();
+        _aiService = new Mock<IAiService>();
+        _storageService = new Mock<IStorageService>();
+        _candidateQueries = new Mock<ICandidateQueries>();
+
+        _service = new CandidateService(
+            _repository.Object,
+            _aiService.Object,
+            _vacancyRepository.Object,
+            _storageService.Object,
+            _candidateQueries.Object
+        );
+    }
+
+    [Fact]
+    public async Task RegisterCandidateAsync_Should_ThrowException_When_VacancyNotFound()
+    {
+        var request = new CreateCandidateRequest { JobVacancyId = Guid.NewGuid() };
+
+        _vacancyRepository.Setup(repo => repo.GetByIdAsync(request.JobVacancyId))
+            .ReturnsAsync((JobVacancy)null);
+
+        var exception = await Assert
+            .ThrowsAsync<KeyNotFoundException>(() => _service.RegisterCandidateAsync(request));
+        Assert.Equal($"Vacancy {request.JobVacancyId} not found", exception.Message);
+    }
+
+    [Fact]
+    public async Task RegisterCandidateAsync_Should_SaveCandidate_When_DataIsValid()
+    {
+        var request = new CreateCandidateRequest
+        {
+            JobVacancyId = Guid.NewGuid(),
+            FirstName = "Jane",
+            LastName = "Doe",
+            Email = "jane.doe@example.com",
+            ResumeText = "I am a fantastic C# developer."
+        };
+
+        var fakeVacancy = new JobVacancy("Fake Title", "Fake Description", "user123");
+
+        typeof(JobVacancy).GetProperty("Id")?.SetValue(fakeVacancy, request.JobVacancyId);
+
+        _vacancyRepository.Setup(repo => repo.GetByIdAsync(request.JobVacancyId))
+            .ReturnsAsync(fakeVacancy);
+
+        var fakeAiResult = new AiAnalysisResult
+            (
+                "Jane",
+                "Doe",
+                95,
+                "Excellent match.",
+                new List<string> { "C#", "React" },
+                new List<string> { "C#", "React" },
+                new List<string> { "C#", "React" }
+                );
+
+        _aiService.Setup(x => x.EvaluateCandidateAsync(fakeVacancy, request.ResumeText))
+            .ReturnsAsync(fakeAiResult);
+
+        await _service.RegisterCandidateAsync(request);
+        _repository.Verify(repo => repo.AddAsync(It.IsAny<Candidate>()), Times.Once);
+    }
+}
---
+++ SmartRecruiter.Application.Tests/Validators/CreateCandidateValidatorTests.cs
@@ -0,0 +1,64 @@
+﻿using FluentValidation;
+using FluentValidation.TestHelper;
+using SmartRecruiter.Application.DTOs;
+using SmartRecruiter.Application.Validators;
+
+namespace SmartRecruiter.Application.Tests.Validators;
+
+public class CreateCandidateValidatorTests
+{
+    private readonly CreateCandidateValidator _validator;
+    
+    public CreateCandidateValidatorTests()
+    {
+        _validator = new CreateCandidateValidator();
+    }
+    
+    [Fact]
+    public void Should_Not_Have_Error_When_Model_Is_Valid()
+    {
+        var request = new CreateCandidateRequest
+        {
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            JobVacancyId = Guid.NewGuid()
+        };
+
+        var result = _validator.TestValidate(request);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData(null)]
+    public void Should_Have_Error_When_FirstName_Is_Empty(string invalidFirstName)
+    {
+        var request = new CreateCandidateRequest
+        {
+            FirstName = invalidFirstName,
+            LastName = "",
+            Email = "john.doe@example.com",
+            JobVacancyId = Guid.NewGuid()
+        };
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.FirstName)
+            .WithErrorMessage("Name can't be empty");
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_Email_Is_Invalid()
+    {
+        var request = new CreateCandidateRequest
+        {
+            FirstName = "Name",
+            LastName = "lastName",
+            Email = "john.doe",
+            JobVacancyId = Guid.NewGuid()
+        };
+
+        var result = _validator.TestValidate(request);
+        result.ShouldHaveValidationErrorFor(x => x.Email)
+            .WithErrorMessage("Enter correct email");
+    }
+}
